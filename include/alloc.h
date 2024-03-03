85c8b60acbc6ec82c6e904405230dc23e9dd57dd
huoweiyuan <huoweiyuan@foxmail.com>
2024-03-03 19:05:49 +0800

    add alloc

--- include/alloc.h
+++ include/alloc.h
@@ -1,28 +1,38 @@
 #ifndef ALLOC_H
 #define ALLOC_H
 
+#include <atomic>
+#include <mutex>
+
 namespace y 
 {
+  class Allocator;
+  extern Allocator *g_allocator;
+  class Allocator
+  {
+  protected:
+    ushort EXTRA_MEM_SIZE = 0;
 
-class Allocator
-{
- protected:
-  ushort EXTRA_MEM_SIZE = 0;
+  private:
+    // for new/delete
+    static constexpr const short OBJ_NUM_HEAD_SIZE = 8;
+    static constexpr const short OBJ_PTR_HEAD_SIZE = 8;
+    static constexpr const short READ_OBJ_NUM_OFFSET = -(OBJ_NUM_HEAD_SIZE) - (OBJ_PTR_HEAD_SIZE);
+    static constexpr const short READ_OBJ_PTR_OFFSET = -(OBJ_PTR_HEAD_SIZE);
 
- private:
-  // for new/delete
-   static constexpr const short OBJ_NUM_HEAD_SIZE = 8;
-   static constexpr const short OBJ_PTR_HEAD_SIZE = 8;
-   static constexpr const short READ_OBJ_NUM_OFFSET = -(OBJ_NUM_HEAD_SIZE) -(OBJ_PTR_HEAD_SIZE);
-   static constexpr const short READ_OBJ_PTR_OFFSET = -(OBJ_PTR_HEAD_SIZE);
+  private:
+    static std::mutex s_mutex;
+    std::atomic_bool m_as_default_allocator;
+    friend void g_allocator_init(Allocator *);
 
- private:
-   virtual char* alloc_memory(std::size_t size)
-   {
-     return static_cast<char*>(::malloc(size));
-   }
-   virtual char* realloc_memory(void *ptr, size_t size)
-   {
+  private:
+    virtual char *
+    alloc_memory(std::size_t size)
+    {
+      return static_cast<char *>(::malloc(size));
+    }
+    virtual char *realloc_memory(void *ptr, size_t size)
+    {
       return static_cast<char*>(::realloc(ptr, size));
    }
    virtual void free_memory(void *ptr)
@@ -96,8 +106,16 @@ class Allocator
    }
 
  public:
-   Allocator() noexcept = default;
-   virtual ~Allocator() noexcept = default;
+   Allocator() noexcept
+   : m_as_default_allocator(false)
+   {}
+   Allocator(bool as_default_allocator) noexcept
+   : m_as_default_allocator(false)
+   {
+     if (as_default_allocator)
+       set_as_default_allocator();
+   }
+   virtual ~Allocator() noexcept;
 
  public:
    static Allocator* get_allocator_ptr(void *ptr) {
@@ -106,35 +124,89 @@ class Allocator
      return *(reinterpret_cast<Allocator**>(
          static_cast<char*>(ptr) + READ_OBJ_PTR_OFFSET));
    }
+
+   void set_as_default_allocator()
+   {
+     std::lock_guard<std::mutex> lk(s_mutex);
+     if (g_allocator)
+       g_allocator->m_as_default_allocator = false;
+     m_as_default_allocator = true;
+     g_allocator = this;
+   }
+
 };
 
-inline void* operator new (size_t size, Allocator *alloc) noexcept
+std::mutex Allocator::s_mutex;
+
+Allocator  g_default_allocator(true);
+y::Allocator *g_allocator = &g_default_allocator;
+
+Allocator::~Allocator()
+{
+  if (m_as_default_allocator) {
+    std::lock_guard<std::mutex> lk(s_mutex);
+    m_as_default_allocator = false;
+    g_allocator = &g_default_allocator;
+    g_default_allocator.m_as_default_allocator = true;
+  }
+}
+
+}
+
+void* operator new (size_t size, y::Allocator *alloc) noexcept
 {
   return alloc->alloc(size);
 }
 
-inline void* operator new[] (size_t size, Allocator *alloc) noexcept
+void* operator new[] (size_t size, y::Allocator *alloc) noexcept
 {
   return alloc->alloc(size);
 }
 
-inline void operator delete(void *pt) noexcept {
-  Allocator *allocator = nullptr; 
-  if (ptr == nullptr ||
-    (allocator = Allocator::get_allocator_ptr(ptr)) == nullptr)
-    return;
-  allocator->free(ptr);
-}
-
-inline void operator delete[](void *ptr) noexcept
+void* operator new(std::size_t size) noexcept
 {
-  Allocator *allocator = nullptr; 
+  return y::g_allocator->alloc(size);
+}
+ 
+void* operator new[](std::size_t size)
+{
+  return y::g_allocator->alloc(size);
+}
+ 
+void operator delete(void* ptr) noexcept
+{
+  y::Allocator *allocator = nullptr; 
   if (ptr == nullptr ||
-    (allocator = Allocator::get_allocator_ptr(ptr)) == nullptr)
+    (allocator = y::Allocator::get_allocator_ptr(ptr)) == nullptr)
+    return;
+  allocator->free(ptr);
+}
+ 
+void operator delete(void* ptr, std::size_t size) noexcept
+{
+  y::Allocator *allocator = nullptr;
+  if (ptr == nullptr ||
+      (allocator = y::Allocator::get_allocator_ptr(ptr)) == nullptr)
     return;
   allocator->free(ptr);
 }
 
+void operator delete[](void* ptr) noexcept
+{
+  y::Allocator *allocator = nullptr; 
+  if (ptr == nullptr ||
+    (allocator = y::Allocator::get_allocator_ptr(ptr)) == nullptr)
+    return;
+  allocator->free(ptr);
+}
+ 
+void operator delete[](void* ptr, std::size_t size) noexcept
+{
+  y::Allocator *allocator = nullptr;
+  if (ptr == nullptr ||
+      (allocator = y::Allocator::get_allocator_ptr(ptr)) == nullptr)
+    return;
+  allocator->free(ptr);
 }
 
 #endif// ALLOC_H
