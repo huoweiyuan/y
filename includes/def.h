03d3f25af61d4339b36ccc235f0346fd95fd70fb
huoweiyuan <huoweiyuan@foxmail.com>
2024-06-06 22:41:02 +0800

    add macro

--- includes/def.h
+++ includes/def.h
@@ -1,12 +1,19 @@
 #ifndef Y_DEF_H
 #define Y_DEF_H
 
-#define SUCCESS (0)
-#define FAILED (-1)
+#define Y_SUCCESS (0)
+#define Y_FAILED (-1)
+#define Y_IS_SUCCESS(expr) (expr == Y_SUCCESS)
+#define Y_IS_FAILED(expr) (expr == Y_FAILED)
 
+/* define base type */
 namespace y {
-  using __ushort = unsigned short;
+using __ushort = unsigned short;
 }
 
+/* define return type */
+namespace y {
+using RET = int;
+}
 
 #endif // Y_DEF_H
